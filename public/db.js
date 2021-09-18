610888ecb21b4692847ea54150b0c90de85fee2e
Ishya Casukhela <casukhelai@gmail.com>
2021-09-18 16:23:52 -0400

    finished db.json

--- public/db.js
+++ public/db.js
@@ -22,3 +22,51 @@ request.onerror = function(event) {
     console.log('Oops!' + event.target.errorCode);
 }
 
+function saveRecord(record){
+    // creates the transaction on PENDING db and has readwrite access
+    const transaction = db.transaction(['pending'], 'readwrite');
+
+    // access pending object store
+    const store = transaction.objectStore('pending');
+
+    // add record to the store
+    store.add(record);
+
+}
+
+function checkDatabase() {
+    // open transaction of pending db
+    const transaction = db.transaction(['pending'], 'readwrite');
+
+    // access pending object store
+    const store = transaction.objectStore('pending');
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                header: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                // success? open transaction on the PENDING db
+                const transaction = db.transaction(['pending'], 'readwrite');
+
+                // access pending object store
+                const store = transaction.objectStore('pending');
+
+                // clear items in store
+                store.clear();
+            })
+        }
+    };
+}
+
+// listen for when app comes online
+window.addEventListener("online", checkDatabase);
