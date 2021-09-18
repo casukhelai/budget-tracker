4d0abe62ac64b7b020b679508f6162a08b12b948
Ishya Casukhela <casukhelai@gmail.com>
2021-09-18 15:43:11 -0400

    added onerror

--- public/db.js
+++ public/db.js
@@ -4,5 +4,21 @@ const request = indexedDB.open("budget", 1);
 request.onupgradeneeded = function(event) {
     // create object "pending"...set autoIncrement = true
     const db = event.target.result;
-    db.createObjectStore('pending', { autoIncrement: true})
+    db.createObjectStore('pending', { autoIncrement: true});
+};
+
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    // check if app is online
+    if(navigator.onLine){
+        checkDatabase();
+    }
 }
+
+// in the event an error occurs, throw an error
+request.onerror = function(event) {
+    console.log('Oops!' + event.target.errorCode);
+}
+
